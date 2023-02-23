5baba96d29dc8a672cbbe017214925ae1a773c9f
RaresCiuchina2DAM <rarciu763@alu.centroafuera.es>
2023-02-23 13:30:51 +0100

    Mejor mejor

--- app/src/main/java/com/example/yonkiflix/SeriesActivity.kt
+++ app/src/main/java/com/example/yonkiflix/SeriesActivity.kt
@@ -13,12 +13,12 @@ class SeriesActivity : AppCompatActivity() {
 
         val series = Array<Int>(init = {0}, size = 1)
         series[0]=R.drawable.aqnhqv
-        series[1]=R.drawable.CCAVM
+        series[1]=R.drawable.ccavm
         series[2]=R.drawable.chernobyl
         series[3]=R.drawable.elpueblo
         series[4]=R.drawable.emilyenparis
         series[5]=R.drawable.futurama
-        series[6]=R.drawable.losSimpsons
+        series[6]=R.drawable.lossimpsons1
         series[7]=R.drawable.laschicasdelcable
         series[8]=R.drawable.disney9
         series[9]=R.drawable.machosalfa
